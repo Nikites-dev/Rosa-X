ce257a4a50b86c00384251c0c0afcd8e0be11f52
Петров Никита <Petovn.xz@mail.ru>
2023-05-15 12:58:42 +0300

    add mud blazor

--- GinSki/Program.cs
+++ GinSki/Program.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,6 +36,19 @@ builder.Services.AddSingleton<SignIn>();
 builder.Services.AddSingleton<RentalInventory>();
 builder.Services.AddSingleton<InventoryComponent>();
 builder.Services.AddSingleton<AddInventory>();
+
+builder.Services.AddMudServices(config =>
+{
+    config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomLeft;
+
+    config.SnackbarConfiguration.PreventDuplicates = false;
+    config.SnackbarConfiguration.NewestOnTop = false;
+    config.SnackbarConfiguration.ShowCloseIcon = true;
+    config.SnackbarConfiguration.VisibleStateDuration = 10000;
+    config.SnackbarConfiguration.HideTransitionDuration = 500;
+    config.SnackbarConfiguration.ShowTransitionDuration = 500;
+    config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+});
 
 
 
